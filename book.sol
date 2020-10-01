25ea9d9ac2e58eb4c3f3805fbc22aa95d81cf7a0
maxxflyer <maxxflyer@gmail.com>
2020-10-01 14:08:50 +0200

    Create book.sol

---
+++ book.sol
@@ -0,0 +1,26 @@
+contract Book {
+
+    address[] public doors;
+    mapping(address => bool)public isModule;
+    address master;
+    
+    function setDoor(address tkn)public returns(bool){
+        require(isModule[msg.sender]||(msg.sender==master));
+        doors.push(tkn);
+        return true;
+    }
+    
+    
+    function setModule(address tkn)public returns(bool){
+        require(isModule[msg.sender]||(msg.sender==master));
+        isModule[tkn]=true;
+        return true;
+    }
+    
+    function setMaster(address mstr)public returns(bool){
+        require((msg.sender==master)||(isModule[msg.sender]));
+        master=mstr;
+        return true;
+    }
+    
+}
