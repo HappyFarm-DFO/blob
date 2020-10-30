2463b970326a6579d0ac84509a223ef3f4b4b2ea
maxxflyer <maxxflyer@gmail.com>
2020-10-30 17:09:47 +0100

    Update 721blob.sol

--- 721blob.sol
+++ 721blob.sol
@@ -1,3 +1,51 @@
+contract HappyBox{
+    
+    uint public code=2;
+    
+    address[] public modules_list;
+    mapping(address => bool)public modules;
+    address public master;
+    
+    constructor() public{
+        master=msg.sender;
+    }
+    
+    function ship(address tkn,uint amount,address destination) public returns(bool){
+        require(modules[msg.sender]);
+        require(ERC20(tkn).transfer(destination, amount));
+        return true;
+    } 
+    
+    function shipNFT(address tkn,uint id,address destination) public returns(bool){
+        require(modules[msg.sender]);
+        require(ERC721(tkn).Transfer(address(this),destination, id));
+        return true;
+    } 
+    
+    //mode 1 = install module
+    //mode 2 = set master
+    //mode 3 = enable module
+    //mode 4 = pull token
+    function set(address tkn,bool what,uint mode)public returns(bool){
+         require((msg.sender==master)||(modules[msg.sender]));
+        if(mode==1){
+            require(MVMCertList(0x5599AF9b3Ab7069e07f546F736c43Ba848a07E69).isModule(tkn));
+            modules[tkn]=true;
+            modules_list.push(tkn);
+        }else if(mode==2){
+                master=tkn;
+        }else if(mode==3){
+            require(MVMCertList(0x5599AF9b3Ab7069e07f546F736c43Ba848a07E69).isModule(tkn));
+                modules[tkn]=what;
+        }else if(mode==4){
+              ERC20 token=ERC20(tkn);
+                ERC20(tkn).transfer(master,token.balanceOf(address(this)));
+        }
+        return true;
+    }
+    
+}
+
 contract ERC721Ticket_1_0_Factory{
     
         uint public code=3;
