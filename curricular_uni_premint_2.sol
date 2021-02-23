5af0e9765f082735c72e72d971afb9d383d7c08b
maxxflyer <maxxflyer@gmail.com>
2021-02-23 14:33:04 +0100

    Update curricular_uni_premint_2.sol

--- curricular_uni_premint_2.sol
+++ curricular_uni_premint_2.sol
@@ -2,21 +2,21 @@ pragma solidity ^0.7.4;
 
 contract DirectCurricularFactory {
     address private _collection;
-    address public curricularECO=0xdA1Ec8F2Fb47e905079663bCEA69f1a2B0;
+    address public curricularECO=0xdA1Ec8F2Fb47e905079663bCEA69f1a2B010f2D3;
     
     function init() public {
         require(_collection == address(0), "Init already called!");
         _collection = msg.sender;
     }
     
-    function mint(string name,string symbol,string uri) public {
+    function mint(string memory name,string memory symbol,string memory uri) public {
         require(msg.sender==curricularECO,"permission required");
-        (,address tokenAddress) = IEthItem(_collection).mint(1000000, name, symbol, uri, false);
+        IEthItem(_collection).mint(1000000, name, symbol, uri, false);
     }
 
     function send(address token,address to)payable public {
-        IERC20 token=IERC20(token);
-        token.transfer(to, 1));
+        IERC20 _token=IERC20(token);
+        _token.transfer(to, 1);
     }
     
     function setOwner(address owner)payable public {
