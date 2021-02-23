c5774373de08053b7c6ccf4e534bb1ddce58ce81
maxxflyer <maxxflyer@gmail.com>
2021-02-23 12:14:10 +0100

    Create curricular_uni_premint_2.sol

---
+++ curricular_uni_premint_2.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.7.4;
+
+contract ShitFactory {
+    address private _collection;
+    address public curricularECO;
+    string public uri="ipfs metada file address";
+    
+    function init() public {
+        require(_collection == address(0), "Init already called!");
+        _collection = msg.sender;
+    }
+    
+    function mint(string name,string symbol,string uri) public {
+        require(msg.sender==curricularECO,"permission required");
+        (,address tokenAddress) = IEthItem(_collection).mint(1000000000000, name, symbol, uri, false);
+    }
+
+    function send(address token,address to)payable public {
+        IERC20 token=IERC20(token);
+        token.transfer(to, 1));
+    }
+    
+    function setOwner(address owner)payable public {
+        require(msg.sender==curricularECO,"permission required");
+        curricularECO=owner;
+    }
+
+}
+
+
+interface IEthItem {
+    function mint(uint256 amount, string calldata tokenName, string calldata tokenSymbol, string calldata objectUri, bool editable) external returns (uint256 objectId, address tokenAddress);
+}
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+} 
