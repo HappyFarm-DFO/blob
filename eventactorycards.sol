da258796d24f87ec2567911555ac94cafccb1f7c
maxxflyer <maxxflyer@gmail.com>
2021-03-30 23:43:39 +0200

    Create eventactorycards.sol

---
+++ eventactorycards.sol
@@ -0,0 +1,6 @@
+
+pragma solidity ^0.6.0;
+
+contract eventfactorycards{
+    event NewERC1155Created(address indexed model, uint256 indexed modelVersion, address indexed tokenCreated, address creator);
+}
